abc7ba6e5887ad6557afb17ef7763b95ecc0d444
alex <sasha-ponom@mail.ru>
2023-07-21 16:43:11 +0500

    Add -A

---
+++ build/cpp_pubsub/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/alex/Documents/student_promobot/cpp_pubsub
+# Build directory: /home/alex/Documents/student_promobot/build/cpp_pubsub
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
