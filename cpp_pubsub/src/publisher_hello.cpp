076b9f44168d467eac59e6a262098209c34a47bf
alexponomisnotbad <139641713+alexponomisnotbad@users.noreply.github.com>
2023-07-23 23:40:47 +0500

    Add files via upload

--- cpp_pubsub/src/publisher_hello.cpp
+++ cpp_pubsub/src/publisher_hello.cpp
@@ -8,22 +8,17 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
-int c_timeout_ms;
+int uint16_t const c_timeout_ms = 2;
 class MinimalPublisher : public rclcpp::Node
 {
 public:
 
-  static const char* _topic = "topic"
-  static const size_t _size = 10
   MinimalPublisher()
-  : Node("minimal_publisher"), _ncount(0)
+  : Node("minimal_publisher")
   {
-    _ppublisher = this->create_publisher<std_msgs::msg::String>(_topic, _size);
-    { 
     using namespace std::chrono_literals;
     _ptimer = this->create_wall_timer(
-    std::chrono::seconds(c_timeout_ms) , std::bind(&MinimalPublisher:: _timer_callback, this));
-    }
+    std::chrono::seconds(c_timeout_ms) , std::bind(&MinimalPublisher:: timer_callback, this));
   }
   private:
     void timer_callback()
@@ -31,16 +26,12 @@ public:
       auto message = std_msgs::msg::String();
       message.data = "Hello, I am alive! ";
       RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-      _ppublisher->publish(message);
       std::this_thread::sleep_for(std::chrono::seconds(c_timeout_ms));
     }
     rclcpp::TimerBase::SharedPtr _ptimer;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr _ppublisher;
-    size_t _ncount;
 };
 int main(int argc, char * argv[])
 {
-  c_timeout_ms = 2
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<MinimalPublisher>());
   rclcpp::shutdown();
