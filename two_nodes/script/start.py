b4c6d593a1270721b129e36ff70b0aec9ad769fd
alexponomisnotbad <139641713+alexponomisnotbad@users.noreply.github.com>
2023-08-07 16:56:20 +0500

    Add files via upload

---
+++ two_nodes/script/start.py
@@ -0,0 +1,7 @@
+
+import subprocess
+
+command = 'bash -c "cd ~/ros2_ws && . install/setup.bash && ros2 launch two_nodes two_nodes_launch.py"'
+
+script = subprocess.Popen(command, shell = True)
+script.wait()
