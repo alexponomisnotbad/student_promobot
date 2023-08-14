b85332b99a7e517a84184c4fc0a60ec8fa642337
alexponomisnotbad <139641713+alexponomisnotbad@users.noreply.github.com>
2023-08-14 16:39:44 +0500

    Update two_node.hpp

--- two_nodes/hpp/two_node.hpp
+++ two_nodes/hpp/two_node.hpp
@@ -7,21 +7,24 @@
 static constexpr char * c_topic = "publisher";
 static constexpr char * c_subtopic = "subscriber";
 static const uint32_t c_test_data = 999;
-uint8_t _twonode_queue_size_prm = 10;
- uint8_t _twonode_member_queue_size = 0;
+static constexpr char * c_queue_parameter = "queue_parameter"
+
 class TwoNode : public rclcpp::Node
 {
 public:
   TwoNode()
   : Node("second_node")
   {
-    this->declare_parameter("_twonode_queue_parameter", _twonode_queue_size_prm);
-    
-    _twonode_member_queue_size = this->get_parameter("_twonode_queue_parameter").as_int();
+    this->declare_parameter(c_queue_parameter, c_queue_size_prm);
+    _twonode_member_queue_size = this->get_parameter(c_queue_parameter).as_int();
     _psubscription = this->create_subscription<std_msgs::msg::String>(
-      c_topic, _twonode_member_queue_size, std::bind(&TwoNode::topicCallback, this, std::placeholders::_1));
-    _ppublisher = this->create_publisher<std_msgs::msg::Int32>(c_subtopic, _twonode_member_queue_size);
+      c_topic, queue_size, std::bind(&TwoNode::topicCallback, this, std::placeholders::_1));
+    _ppublisher = this->create_publisher<std_msgs::msg::Int32>(c_subtopic, queue_size);
   }
+
+public:
+const uint8_t c_queue_size_prm = 10;
+uint8_t queue_size = 0;
 
 private:
   void topicCallback(const std_msgs::msg::String & msg) const
